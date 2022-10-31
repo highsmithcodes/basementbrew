d4fe4a0a6259c0b7164030a8e8ee69f944557b63
Highsmith Codes <ezrahighsmith@gmail.com>
2022-10-30 20:57:57 -0600

    add schema to gitignore

---
+++ server/models/schema.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE users (
+    id uuid PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR NOT NULL
+);
+CREATE TABLE posts (
+    pid uuid PRIMARY KEY,   
+    title VARCHAR(255),
+    body VARCHAR,
+    user_id uuid REFERENCES users(id),
+    author VARCHAR REFERENCES users(username),
+    date_created TIMESTAMP
+);
