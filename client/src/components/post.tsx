ef36df7a6fbf74f7ffbfd713133e18196eabab29
Highsmith Codes <ezrahighsmith@gmail.com>
2022-12-20 07:22:31 -0700

    single post

--- client/src/components/post.tsx
+++ client/src/components/post.tsx
@@ -4,35 +4,36 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
 import { auth, db, storage } from "../config/firebase";
 
-export interface Post {
-    id: string;
-    userId: string;
-    title: string;
-    description: string;
-    username: string;
-    imageUrl: string;
-}
+// export interface Post {
+//     id: string;
+//     userId: string;
+//     title: string;
+//     description: string;
+//     username: string;
+//     imageUrl: string;
+// }
 
 export const Post = () => {
     const { id } = useParams();
     const [user] = useAuthState(auth);
-    const [post, setPost] = useState<Post[] | null>(null);
-    const postRef = collection(db, "post");
-    const postDoc = query(postRef, where("postId", "==", id));
-        
-    const getPosts = async () => {
-        try{
-            // const postToShow = await getDocs(postDoc)
-           console.log(id)
-            // setPost(postToShow.docs.map((doc) => ({id: doc.data().id ,userId: doc.data().userId, title: doc.data().title, description: doc.data().description, username: doc.data().username, imageUrl: doc.data().imageUrl})));
-        } catch (err){
-            err
-        }
+    // const [post, setPost] = useState<Post[] | null>(null);
+    // const postRef = collection(db, "post");
+    // const postDoc = query(postRef, where("postId", "==", id));
+    const [movie, setMovie] = useState([]);
+    const getPost = async () => {
+        const postToShow = await getDocs(collection(db, "posts"))
+        const firebaseMovies = [];
+        querySnapshot.forEach((doc) => {
+            firebaseMovies.push({...doc.data(), id:doc.id});
+        });
+        const movieData = firebaseMovies.filter((item) => item.id === id);
+        setMovie(movieData[0])
+
     }
 
     useEffect(() => {
-        getPosts();
-    }, []);
+        getPost();
+    }, [id]);
   
 
     return (
