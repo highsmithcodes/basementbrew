240fa138d04cbde754834ddea72a8ff2e942666d
Highsmith Codes <ezrahighsmith@gmail.com>
2024-04-13 04:34:37 +0000

    remove aws

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,18 +3,14 @@ import { useEffect } from "react";
 
 
 import { useNavigate, useLocation } from 'react-router';
-import { dynamoDB } from "../configs/dynamoDBConfig";
 
 export function Login() {
   const location = useLocation();
   const navigate = useNavigate();
-  console.log("All Environment Variables:", process.env.AWS_PROJECT_REGION);
-  let from = location.state?.from?.pathname || '/dashboard';
-
 
   return (
     <section className="bg-gray-50 hero relative overflow-hidden px-4 py-32 flex lg:h-screen items-center content-center justify-center">
-   
+    
     </section>
   );
 }
